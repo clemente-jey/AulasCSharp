9febcb5dfc5212bd4771de8ff39b19e7bce632aa
jonas.jcsantos <jonas.jcsantos@TIT0533388W10-1>
2025-02-25 19:27:13 -0300

    Adicionar arquivos de projeto.

---
+++ Aula01/Aula01/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Hello, World!\n");
+
+Console.Write("I'm Jey \n");
+Console.WriteLine("Hello, \nWorld!");
+Console.Write("Boa tarde!\n");
+
+string firstName = "Jonas";
+string lastName = "Clemente";
+
+Console.WriteLine("Meu nome completo é " + firstName  +" "+ lastName);
+
+int idade = 22;
+
+
+Console.WriteLine($"Meu nome completo é {firstName} {lastName} e a minha idade é {idade} anos");
+
+Console.WriteLine("Escreva seu nome");
+string nome = Console.ReadLine();
+Console.WriteLine($"Olá {nome}, boa noite");
