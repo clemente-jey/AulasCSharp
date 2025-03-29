e15cec771b1f2727ab14a68a13b2089a01f02b4b
clemente-jey <jonasclemente.contato@gmail.com>
2025-03-28 21:38:37 -0300

    Arrumei e está rodando

--- SistemaPontoEletronico/SistemaPontoEletronico/SistemaPontoEletronico/frmPrincipal.Designer.cs
+++ SistemaPontoEletronico/SistemaPontoEletronico/SistemaPontoEletronico/frmPrincipal.Designer.cs
@@ -38,7 +38,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(395, 144);
+            this.button1.Location = new System.Drawing.Point(356, 130);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(144, 23);
             this.button1.TabIndex = 0;
@@ -48,7 +48,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(395, 222);
+            this.button2.Location = new System.Drawing.Point(356, 222);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(144, 23);
             this.button2.TabIndex = 1;
