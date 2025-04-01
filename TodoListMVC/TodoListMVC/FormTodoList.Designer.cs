cf2033f68dd7e9470d2aec8e8b8c13f85245262c
clemente-jey <jonasclemente.contato@gmail.com>
2025-04-01 20:24:08 -0300

    Finalizei o código mas ainda não está rodando, falta fazer uma tratativa de erros

--- TodoListMVC/TodoListMVC/FormTodoList.Designer.cs
+++ TodoListMVC/TodoListMVC/FormTodoList.Designer.cs
@@ -28,13 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtTarefa = new System.Windows.Forms.TextBox();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.listBoxTarefas = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtTarefa
+            // 
+            this.txtTarefa.Location = new System.Drawing.Point(197, 121);
+            this.txtTarefa.Name = "txtTarefa";
+            this.txtTarefa.Size = new System.Drawing.Size(146, 20);
+            this.txtTarefa.TabIndex = 0;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(397, 121);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(181, 20);
+            this.btnAdicionar.TabIndex = 1;
+            this.btnAdicionar.Text = "Adicionar Tarefa";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            // 
+            // listBoxTarefas
+            // 
+            this.listBoxTarefas.FormattingEnabled = true;
+            this.listBoxTarefas.Location = new System.Drawing.Point(223, 202);
+            this.listBoxTarefas.Name = "listBoxTarefas";
+            this.listBoxTarefas.Size = new System.Drawing.Size(120, 95);
+            this.listBoxTarefas.TabIndex = 2;
+            // 
+            // FormTodoList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxTarefas);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.txtTarefa);
+            this.Name = "FormTodoList";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormTodoList_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtTarefa;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.ListBox listBoxTarefas;
     }
 }
 
--- TodoListMVC/TodoListMVC/FormTodoList.cs
+++ TodoListMVC/TodoListMVC/FormTodoList.cs
@@ -10,11 +10,35 @@ using System.Windows.Forms;
 
 namespace TodoListMVC
 {
-    public partial class FormTodoList: Form
+    public partial class FormTodoList : Form
     {
+        private TodoController _controller;
+
         public FormTodoList()
         {
             InitializeComponent();
+            _controller = new TodoController();
+            AtualizarLista();
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtTarefa.Text))
+            {
+                _controller.AdicionarTarefa(txtTarefa.Text);
+                txtTarefa.Clear();
+                AtualizarLista();
+            }
+        }
+
+        private void AtualizarLista()
+        {
+            listBoxTarefas.Items.Clear();
+            foreach (Tarefa tarefa in _controller.ObterTarefas())
+            {
+                listBoxTarefas.Items.Add(tarefa.Descricao);
+            }
         }
     }
+
 }
